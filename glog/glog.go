cbb1856a6664dd39f5e7a3fb7aa6f1ff40ef606a
robertzml <robertzml@126.com>
2020-11-21 10:15:21 +0800

    modify glog.

--- glog/glog.go
+++ glog/glog.go
@@ -1,28 +1,34 @@
 package glog
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/robertzml/Gorgons/base"
-	"io"
-	"os"
-	"path/filepath"
+	"github.com/streadway/amqp"
 	"time"
 )
 
-// 日志 channel
-var GlogCh  chan *GlogPacket
+const (
+	// 日志显示系统名称
+	systemName = "Gorgons"
+)
 
+// 日志 channel
+var logChan  chan *packet
 
 // 日志数据包
-type GlogPacket struct {
-	// 日志级别
+type packet struct {
+	// 日志级别 0-5
 	Level  		int
 
-	// 模块名称
-	PackageName	string
+	// 系统名称
+	System 		string
 
-	// 标题
-	Title		string
+	// 模块名称
+	Module		string
+
+	// 操作名称
+	Action		string
 
 	// 日志内容
 	Message		string
@@ -30,72 +36,68 @@ type GlogPacket struct {
 
 // 初始化日志
 func InitGlog() {
-	folder, _ := filepath.Abs("./log")
-	createDir(folder)
-
-	GlogCh = make(chan *GlogPacket, 10)
+	logChan = make(chan *packet, 10)
 }
 
 // 写日志到channel 中
-func Write(level int, packageName string, title string, message string) {
-	packet := GlogPacket{Level: level, PackageName: packageName, Title: title, Message: message}
-	GlogCh <- &packet
+// {"exception", "error", "waring", "info", "debug", "verbose"}
+func Write(level int, module string, action string, message string) {
+	pak := packet{Level: level, System: systemName, Module: module, Action: action, Message: message}
+	logChan <- &pak
 }
 
-// 从channel 中获取日志并写入到文件
+// 从channel 中获取日志并写入到队列
 func Read() {
+	rmConnection, err := amqp.Dial(base.DefaultConfig.RabbitMQAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	rbChannel, err := rmConnection.Channel()
+	if err != nil {
+		panic(err)
+	}
+
 	defer func() {
+		rmConnection.Close()
+		rbChannel.Close()
 		fmt.Println("log service is close.")
 	}()
 
-	levels := [...]string{"exception", "error", "waring", "info", "debug"}
+	queue, err := rbChannel.QueueDeclare("LogQueue", true, false, false, false, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	levels := [...]string{"exception", "error", "waring", "info", "debug", "verbose"}
 
 	for {
-		packet := <- GlogCh
+		pak := <- logChan
 
-		if packet.Level > base.DefaultConfig.LogLevel {
+		if pak.Level > base.DefaultConfig.LogLevel {
 			continue
 		}
 
-		now := time.Now()
-		filename := fmt.Sprintf("./log/%d%02d%02d.log", now.Year(), now.Month(), now.Day())
-		path, _ := filepath.Abs(filename)
+		// 获取日志消息内容
+		jsonData, _ := json.Marshal(pak)
 
-		text := fmt.Sprintf("[%s][%s]-[%s]:[%s]\t%s\n",
-			levels[packet.Level], now.Format("2006-01-02 15:04:05.000"), packet.PackageName, packet.Title, packet.Message)
-
-		if err := writeFile(path, []byte(text)); err != nil {
-			fmt.Println(err)
+		// 推送到 rabbitmq
+		err = rbChannel.Publish("", queue.Name, false, false, amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			ContentType: "text/plain",
+			Body: jsonData,
+		})
+		if err != nil {
+			print(err)
 		}
+
+		// 输出到控制台
 		if base.DefaultConfig.LogToConsole {
+			now := time.Now()
+			text := fmt.Sprintf("[%s][%s]-[%s]:[%s]\t%s\n",
+				levels[pak.Level], now.Format("2006-01-02 15:04:05.000"), pak.Module, pak.Action, pak.Message)
+
 			fmt.Print(text)
 		}
 	}
-}
-
-// 创建文件夹
-func createDir(path string) {
-	_, err := os.Stat(path)
-	if err != nil{
-		if os.IsNotExist(err){
-			_ = os.Mkdir(path, 0744)
-		}
-	}
-}
-
-// 写文件
-func writeFile(filename string, data []byte) error {
-	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0x644)
-	if err != nil {
-		return err
-	}
-	n, err := f.Write(data)
-	if err == nil && n < len(data) {
-		err = io.ErrShortWrite
-	}
-	if err1 := f.Close(); err == nil {
-		err = err1
-	}
-
-	return err
 }
